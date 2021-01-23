8bd8ce1e07f1fc954a80a571954deb7b4406b75b
ayobeen <abdlazeezmumeen@gmail.com>
2021-01-23 03:45:53 +0100

    ...

--- harvis/settings.py
+++ harvis/settings.py
@@ -132,8 +132,8 @@ if DEBUG:
   MEDIA_URL = '/media/'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'havwis.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'info@havwis.com'
 EMAIL_HOST_PASSWORD = 'Havwis25528'
